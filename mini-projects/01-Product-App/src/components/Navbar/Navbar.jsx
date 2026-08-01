ac8d9e80d5a1c2447c8eeb89aad75aa26c98e375
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-08-01 03:49:57 -0700

    feat: add navbar component

---
+++ mini-projects/01-Product-App/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={"/"}>Home Page</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/about"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/products'}>Products</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/product'}>Product</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
